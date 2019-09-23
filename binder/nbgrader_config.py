316e1e0ea4096bfc48f618adb422d2a5b448a009
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-09-23 14:57:44 +0200

    Include default nbgrader configuration file

---
+++ binder/nbgrader_config.py
@@ -0,0 +1,11 @@
+import os
+c = get_config()
+c.CourseDirectory.course_id = 'Info114'
+
+c.Exchange.root = '/public/info-114/exchange'
+# Waiting for nbgader PR #1222
+# c.CourseDirectory.submitted_directory = os.path.join(str(c.Exchange.root), str(c.CourseDirectory.course_id), "submitted")
+c.ExchangeCollect.check_owner=False
+
+c.CourseDirectory.ignore = ['.ipynb_checkpoints', '*.pyc', '__pycache__', 'feedback']
+c.CourseDirectory.max_file_size = 100 # Kb
