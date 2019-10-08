c7b07c9337ba59014294926640a4632c90708b14
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-10-08 09:43:53 +0200

    Use patched nbgrader; setup feedback and autograded directories

--- binder/nbgrader_config.py
+++ binder/nbgrader_config.py
@@ -1,10 +1,12 @@
-import os
+import os.path
 c = get_config()
 c.CourseDirectory.course_id = 'Info114'
 
 c.Exchange.root = '/public/info-114/exchange'
-# Waiting for nbgader PR #1222
-# c.CourseDirectory.submitted_directory = os.path.join(str(c.Exchange.root), str(c.CourseDirectory.course_id), "submitted")
+exchange = os.path.join(str(c.Exchange.root), str(c.CourseDirectory.course_id))
+# Requires nbgader PR #1222 for absolute path
+c.CourseDirectory.submitted_directory = os.path.join(exchange, "submitted")
+c.CourseDirectory.feedback_directory  = os.path.join(exchange, "feedback_generated")
 c.ExchangeCollect.check_owner=False
 
 c.CourseDirectory.ignore = ['.ipynb_checkpoints', '*.pyc', '__pycache__', 'feedback']
