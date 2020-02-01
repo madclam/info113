10b554ca450265c0ee5e535b22753a716a6e9a62
Nicolas M. Thiéry <nthiery@users.sf.net>
2020-02-01 12:19:51 +0100

    Typo, better ignore list

--- binder/nbgrader_config.py
+++ binder/nbgrader_config.py
@@ -4,10 +4,10 @@ c.CourseDirectory.course_id = 'Info114'
 
 c.Exchange.root = '/public/info-114/exchange'
 exchange = os.path.join(str(c.Exchange.root), str(c.CourseDirectory.course_id))
-# Requires nbgader PR #1222 for absolute path
+# Requires nbgrader PR #1222 for absolute path
 c.CourseDirectory.submitted_directory = os.path.join(exchange, "submitted")
 c.CourseDirectory.feedback_directory  = os.path.join(exchange, "feedback_generated")
 c.ExchangeCollect.check_owner=False
 
-c.CourseDirectory.ignore = ['.ipynb_checkpoints', '.nbgrader.log', '*.pyc', '__pycache__', 'feedback', '*~', '.*', 'core*']
+c.CourseDirectory.ignore = ['.ipynb_checkpoints', '*.pyc', '__pycache__', 'feedback', '*~', '.*', 'core*']
 c.CourseDirectory.max_file_size = 1000 # Kb
